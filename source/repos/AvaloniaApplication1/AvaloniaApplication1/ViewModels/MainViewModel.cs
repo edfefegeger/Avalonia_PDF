02660bbcb475c6d7231951389b57b50c2eeb661c
MYXAMOR338 <silova155@gmail.com>
2024-01-26 13:07:42 +0300

    bad pdf exec

--- source/repos/AvaloniaApplication1/AvaloniaApplication1/ViewModels/MainViewModel.cs
+++ source/repos/AvaloniaApplication1/AvaloniaApplication1/ViewModels/MainViewModel.cs
@@ -1,6 +1,42 @@
-﻿namespace AvaloniaApplication1.ViewModels;
+﻿using System.Reactive;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
 
-public class MainViewModel : ViewModelBase
+namespace AvaloniaApplication1.ViewModels
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public class MainViewModel : ReactiveObject
+    {
+        private string? _selectedPdfPath;
+        private readonly Window _mainWindow;
+
+        public MainViewModel(Window mainWindow)
+        {
+            _mainWindow = mainWindow;
+            SelectPdfCommand = ReactiveCommand.CreateFromTask(SelectPdf);
+        }
+
+        public string Greeting => "Welcome to Avalonia!";
+
+        public string? SelectedPdfPath
+        {
+            get => _selectedPdfPath;
+            set => this.RaiseAndSetIfChanged(ref _selectedPdfPath, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> SelectPdfCommand { get; }
+
+        private async Task SelectPdf()
+        {
+            var fileDialog = new OpenFileDialog();
+
+            var selectedFile = await fileDialog.ShowAsync(_mainWindow);
+
+            if (selectedFile != null && selectedFile.Length > 0)
+            {
+                SelectedPdfPath = selectedFile[0];
+            }
+        }
+    }
 }
