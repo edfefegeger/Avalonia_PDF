5238eeb7521c5312a4153150b4603b06baf75bc7
MYXAMOR338 <silova155@gmail.com>
2024-02-03 15:38:58 +0300

    WORKING PDF

--- source/repos/AvaloniaApplication1/AvaloniaApplication1/Views/PdfViewer.axaml.cs
+++ source/repos/AvaloniaApplication1/AvaloniaApplication1/Views/PdfViewer.axaml.cs
@@ -1,13 +1,11 @@
 using Avalonia.Controls;
-using System;
-using System.IO;
-using PdfSharp.Pdf;
-using PdfSharp.Pdf.IO;
-using Avalonia.Media;
-using Avalonia.Layout;
-using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
-using Avalonia.Threading;
+using Avalonia.Media.Imaging;
+using PdfiumViewer;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AvaloniaApplication1.Views
@@ -15,21 +13,12 @@ namespace AvaloniaApplication1.Views
     public partial class PdfViewer : UserControl
     {
         private PdfDocument _pdfDocument;
+        private Avalonia.Media.Imaging.Bitmap _currentBitmap;
         private int _currentPageIndex = 0;
-
-        // Поле и свойство для отслеживания завершения загрузки PDF-файла
         private TaskCompletionSource<bool> _loadPdfTaskCompletionSource = new TaskCompletionSource<bool>();
         public Task LoadPdfTask => _loadPdfTaskCompletionSource.Task;
 
-
-
         public PdfViewer()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-
-
-        private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
@@ -40,11 +29,11 @@ namespace AvaloniaApplication1.Views
             {
                 using (MemoryStream stream = new MemoryStream(pdfContent))
                 {
-                    _pdfDocument = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+                    _pdfDocument = PdfDocument.Load(stream);
+                    _currentBitmap = RenderPdfPage(_currentPageIndex);
                     UpdateDisplayedPage();
                 }
 
-                // Установка значения TaskCompletionSource при завершении загрузки PDF-файла
                 _loadPdfTaskCompletionSource.TrySetResult(true);
             }
             catch (Exception ex)
@@ -54,25 +43,27 @@ namespace AvaloniaApplication1.Views
             }
         }
 
-
-
-        private void UpdateDisplayedPage()
+        private Avalonia.Media.Imaging.Bitmap RenderPdfPage(int pageIndex)
         {
-            var textBlock = this.FindControl<TextBlock>("pdfTextBlock");
-            textBlock.Text = $"Page {_currentPageIndex + 1} of {_pdfDocument.PageCount}";
+            var pdfBitmap = _pdfDocument.Render(pageIndex, 96, 96, PdfRenderFlags.Annotations);
 
-            if (_pdfDocument != null && _currentPageIndex >= 0 && _currentPageIndex < _pdfDocument.PageCount)
+            // Преобразование System.Drawing.Image в Avalonia.Media.Imaging.Bitmap
+            using (MemoryStream memoryStream = new MemoryStream())
             {
-                var page = _pdfDocument.Pages[_currentPageIndex];
-
-                // Set the width of the displayed page
-
-
-                // ... Rest of the code
+                pdfBitmap.Save(memoryStream, ImageFormat.Png);
+                memoryStream.Position = 0;
+                return new Avalonia.Media.Imaging.Bitmap(memoryStream);
             }
         }
 
+        private void UpdateDisplayedPage()
+        {
+            var imageControl = this.FindControl<Avalonia.Controls.Image>("pdfImage");
+            imageControl.Source = _currentBitmap;
 
+            var textBlock = this.FindControl<Avalonia.Controls.TextBlock>("pdfTextBlock");
+            textBlock.Text = $"Страница {_currentPageIndex + 1} из {_pdfDocument.PageCount}";
+        }
 
         public void ScrollPage(int delta)
         {
@@ -83,10 +74,10 @@ namespace AvaloniaApplication1.Views
                 if (newPageIndex != _currentPageIndex)
                 {
                     _currentPageIndex = newPageIndex;
+                    _currentBitmap = RenderPdfPage(_currentPageIndex);
                     UpdateDisplayedPage();
                 }
             }
         }
-
     }
 }
